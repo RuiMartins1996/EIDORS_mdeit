bd194cad633f0d02c95f6747522b09475d45e27d
Rui Martins <martins.rui@ua.pt>
2026-07-02 17:17:32 +0100

    Added absolute GN solver

---
+++ functions/solvers/inverse/inv_solve_absolute_GN_mdeit.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = inv_solve_absolute_GN_mdeit(inv_model, data1)
+%INV_ Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
